9cee42d30ea000f7e84c6c00e77c661ea4915dd2
Arielboim <lgboim@gmail.com>
2024-05-24 12:47:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
         "Flask>=2.0.1",
         "gunicorn>=20.1.0",
         "openai>=0.8.0",
-        "Jinja2>=3.0"
+        "Jinja2>=3.0",
+        "gunicorn"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
